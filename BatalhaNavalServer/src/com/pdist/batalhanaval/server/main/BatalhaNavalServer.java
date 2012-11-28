a22255dd92c8595bf996c019418c678ea655f15a
Ivo Pereira <ivo.g.pereira89@gmail.com>
2012-11-28 03:26:39 +0000

    Server

---
+++ BatalhaNavalServer/src/com/pdist/batalhanaval/server/main/BatalhaNavalServer.java
@@ -0,0 +1,13 @@
+package com.pdist.batalhanaval.server.main;
+
+public class BatalhaNavalServer {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
